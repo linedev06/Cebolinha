2a528218dd0c02ca3236ae9c8f23d9654bc6198c
linedev06 <vitorafelismino@gmail.com>
2023-04-01 02:11:29 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string frasedigitada, frasecebolinha;
+
+Console.Write("Digite uma frase:");
+frasedigitada = Console.ReadLine()!;
+
+frasecebolinha = frasedigitada
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.Write(frasecebolinha);
